cd1bec00c776d24e63c2b83295a5856b3873053d
David Palacios <davidpa7710@gmail.com>
2022-10-05 21:53:39 -0500

    ✨  feat: new animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
       <div className="col mx-5 my-4">
         <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{backgroundImage: 'url("./src/assets/twitter_FbLOLtUUcAAyJFs.jpg")'}}>
           <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-            <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold calendar">Next Race <Icon.ArrowBarRight /></h2>
+            <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold calendar">Previous Race <Icon.ArrowBarRight /></h2>
             <ul className="d-flex list-unstyled mt-auto">
               <li className="me-auto">
                <small>Susuka International Circuit</small>
@@ -99,7 +99,7 @@ const Home = () => {
       <div className="col mx-5 my-4">
         <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{backgroundImage: 'url("./src/assets/twitter_FbLOLtUUcAAyJFs.jpg")'}}>
           <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-            <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold calendar">Next Race <Icon.ArrowBarRight /></h2>
+            <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold calendar"> Upcoming Race <Icon.ArrowBarRight /></h2>
             <ul className="d-flex list-unstyled mt-auto">
               <li className="me-auto">
                <small>Susuka International Circuit</small>
